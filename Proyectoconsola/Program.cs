df1874b16d7ff6506fb5f8617adc28c521f605a3
Kevin de Jes�s Cantu Valenzuela <Kevin_Cantu23@unikino.edu.mx>
2024-07-06 14:21:21 -0700

    Ejercicio con arreglos

--- Proyectoconsola/Program.cs
+++ Proyectoconsola/Program.cs
@@ -5,6 +5,34 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hola C# y GitHub");
+
+
+            string nombre = "Kevin";
+            Console.WriteLine(nombre);
+
+            string[] nombres = new string[4];
+            nombres[0] = "Kevin";
+            nombres[1] = "Gale";
+            nombres[2] = "Cristo";
+            nombres[3] = "Francisco";
+
+            Console.WriteLine(nombres[2]);
+
+            Console.WriteLine();
+            Console.WriteLine("mostrando todos los elementos del arreglo");
+            Console.WriteLine();
+
+            foreach (string nombreAlumno in nombres) 
+            {
+                Console.WriteLine(nombreAlumno);
+
+            }
+
+
+
+
+
+
         }
     }
 }
